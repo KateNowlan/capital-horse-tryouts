9e44672b1c2f4d97ab16ae4d5296f99a5d660c02
KateNowlan <kate.turner26@gmail.com>
2022-10-14 13:05:33 +0000

    correct spelling mistake in env.py file

--- tryouts/settings.py
+++ tryouts/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 import dj_database_url
 if os.path.isfile("env.py"):
     import env
@@ -89,7 +90,7 @@ WSGI_APPLICATION = 'tryouts.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
