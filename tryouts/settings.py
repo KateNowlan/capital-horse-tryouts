51ae1ffabd50f24be0821d03744ee506e3a31967
KateNowlan <kate.turner26@gmail.com>
2022-10-14 09:38:01 +0000

    try to correct procfile

--- tryouts/settings.py
+++ tryouts/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['capital-stud-tryouts2022.herokuapp.com' , 'localhost']
+ALLOWED_HOSTS = ['tryouts.herokuapp.com', 'localhost']
 
 
 # Application definition
